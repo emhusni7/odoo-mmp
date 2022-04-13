5bc47f01e14c2f3d2dfa755c268c6f438960774a
emhusni7 <emhusni77@gmail.com>
2022-04-13 08:14:10 +0700

    code_backend_theme

--- odoo/addons/hr_contract_mmp/models/job.py
+++ odoo/addons/hr_contract_mmp/models/job.py
@@ -1,5 +1,5 @@
 from odoo import models, fields
 
-class Department(models.Model):
-    _inherit = "department"
-    department_code = fields.Char("Code")
+# class Department(models.Model):
+#     _inherit = "department"
+#     department_code = fields.Char("Code")
