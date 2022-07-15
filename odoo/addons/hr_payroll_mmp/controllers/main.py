9cb3e8292b9838924c2c7fc3940a591d2c31b98a
husni <emhusni77@gmail.com>
2022-07-15 15:35:16 +0700

    Pembaruan Data

--- odoo/addons/hr_payroll_mmp/controllers/main.py
+++ odoo/addons/hr_payroll_mmp/controllers/main.py
@@ -7,13 +7,14 @@ class PDFReport(http.Controller):
 
     @http.route(['/hr_payroll_mmp/report/<model("hr.payslip.run"):model>',], type='http', auth="user", csrf=False)
     def generate_report(self,model=None,**args):
-        base_url = request.env['ir.config_parameter'].get_param('web.base.url')
+        url = request.env['ir.config_parameter'].get_param('jasper.url')
+        us_pwd = eval(request.env['ir.config_parameter'].get_param('jasper.user'))
         # attachment_obj = self.env['ir.attachment']
-        url = 'http://localhost:8080/jasperserver/rest_v2/reports'
+
         # url = 'http://localhost:8080/jasperserver/rest_v2/resource'
         report = '/reports/interactive/MMP/payslip.pdf?id=%s'%(model.id)
         # Authorisation credentials:
-        auth = ('jasperadmin', 'jasperadmin')
+        auth = (us_pwd.get('username'), us_pwd.get('password'))
         s = requests.Session()
         r = s.get(url=url + report, auth=auth)
         return  request.make_response(
@@ -26,11 +27,12 @@ class PDFReport(http.Controller):
 
     @http.route(['/hr_payroll_mmp/report_payroll/<model("wiz.payroll.report"):model>', ], type='http', auth="user", csrf=False)
     def report_monthly(self,model=None,**args):
-        url = 'http://localhost:8080/jasperserver/rest_v2/reports'
+        url = request.env['ir.config_parameter'].get_param('jasper.url')
+        us_pwd = eval(request.env['ir.config_parameter'].get_param('jasper.user'))
         # url = 'http://localhost:8080/jasperserver/rest_v2/resource'
         report = '/reports/interactive/MMP/payroll_report_mmp.pdf?year=%s&month=%s&struct_id=%s' % (int(model.year),model.month,model.struct_id.id)
         # Authorisation credentials:
-        auth = ('jasperadmin', 'jasperadmin')
+        auth = (us_pwd.get('username'), us_pwd.get('password'))
         s = requests.Session()
         r = s.get(url=url + report, auth=auth)
         return request.make_response(
